155b102ef58c64e45911cf144ec49fb9628a5105
taha <abootaha118@gmail.com>
2024-05-24 16:25:04 +0330

    change styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 (function($){
 	"use strict";
 	$(window).on('scroll',function() {
-		if ($(this).scrollTop() > 120){  
+		if ($(this).scrollTop() > 120){
 			$('.navbar-area').addClass("is-sticky");
 		}
 		else{
@@ -14,35 +14,6 @@
 		meanScreenWidth: "991"
 	});
 
-    var swiper = new Swiper(".feedbackSwiper", {
-        autoplay: false,
-        direction: "vertical",
-        navigation: {
-            nextEl: ".two",
-            prevEl: ".one",
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-                direction: "horizontal",
-            },
-            576: {
-                slidesPerView: 1,
-                direction: "horizontal",
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 25,
-                direction: "horizontal",
-            },
-            992: {
-                slidesPerView: 2,
-            },
-            1200: {
-                slidesPerView: 2,
-            },
-        },
-    });
 	var swiper = new Swiper(".feedbackSwiperSlides", {
 		loop: true,
         autoplay: false,
